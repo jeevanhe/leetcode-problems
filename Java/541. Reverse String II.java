3bba44179227588d1e4003f90f15c85428f9eab9
Hunsur Eswara <jeevanh@netapp.com>
2017-07-04 20:04:16 -0700

    Reverse string for given k and move

---
+++ Java/541. Reverse String II.java
@@ -0,0 +1,38 @@
+//541. Reverse String II
+// Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+// Example:
+// Input: s = "abcdefg", k = 2
+// Output: "bacdfeg"
+// Restrictions:
+// The string consists of lower English letters only.
+// Length of the given string and k will in the range [1, 10000]
+
+//https://discuss.leetcode.com/topic/82626/java-concise-solution
+
+public class Solution {
+    public String reverseStr(String s, int k) {
+
+        if (s == null)
+            return null;
+
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int i = 0;
+        while(i < n) {
+            int j = Math.min(i + k - 1, n - 1);
+            swap(arr, i, j);
+            i += 2 * k;
+        }
+        return String.valueOf(arr);
+    }
+    private void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+
+}
+
+//Runtime: 7 ms
