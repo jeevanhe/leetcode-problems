236065e648e25a4ce884ee87a20398f7b2c59d09
Hunsur Eswara <jeevanh@netapp.com>
2017-07-07 09:41:14 -0700

    Invert a bianry tree

---
+++ Java/226. Invert Binary Tree.java
@@ -0,0 +1,44 @@
+//226. Invert Binary Tree
+// Invert a binary tree.
+//
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// to
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+// Trivia:
+// This problem was inspired by this original tweet by Max Howell:
+// Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+
+//DFS   //https://discuss.leetcode.com/topic/17012/my-simple-recursion-java-solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
+
+//Runtime: 0 ms
+
+//DFS and BFS iterative method
+//https://discuss.leetcode.com/topic/16039/straightforward-dfs-recursive-iterative-bfs-solutions/5
